3862d51261aa2ee04ebb84f7ec5078ad83153d52
pashkoveval <61497581+pashkoveval@users.noreply.github.com>
2020-05-06 21:21:16 +0300

    Revert "Старые работы"

--- аукцион/js/drop_window.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.serch__btn,.serh__header').click(function (event) {
-      $('.serh__header').toggleClass('serch__activ');
-      $('.serch__btn').toggleClass('serch__activ__drop');
-      $('body').toggleClass('lock');
-    });
-    
-  })
